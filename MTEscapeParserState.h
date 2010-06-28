fc9e698b4e02c934e417543b766c129eb5541db3
Scott M. Kroll <skroll@gmail.com>
2010-06-28 19:35:13 -0400

    Added new properties to MTEscapeParserState.
    Added currentState.
    Added pendingMouseMode.
    Added toggleState.

--- MTEscapeParserState.h
+++ MTEscapeParserState.h
@@ -2,6 +2,11 @@
 
 @interface MTEscapeParserState : NSObject
 {
+	int currentState;
+	int pendingMouseMode;
+	BOOL toggleState;
 }
-
+@property (nonatomic, assign) int currentState;
+@property (nonatomic, assign) int pendingMouseMode;
+@property (nonatomic, assign) BOOL toggleState;
 @end
--- MTEscapeParserState.m
+++ MTEscapeParserState.m
@@ -1,5 +1,7 @@
 #import "MTEscapeParserState.h"
 
 @implementation MTEscapeParserState
-
+@synthesize currentState;
+@synthesize pendingMouseMode;
+@synthesize toggleState;
 @end
